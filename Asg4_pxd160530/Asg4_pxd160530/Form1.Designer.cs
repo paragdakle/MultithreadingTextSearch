546fb05c04db4e5a19595f687c1d5cbabb32194c
pxd160530 <pxd160530@CSLAB77052>
2017-03-08 18:29:51 -0600

    Added FileReader Class. Completed basic Form UI. Started BackgroundWorker file reading procedure.

--- Asg4_pxd160530/Asg4_pxd160530/Form1.Designer.cs
+++ Asg4_pxd160530/Asg4_pxd160530/Form1.Designer.cs
@@ -34,6 +34,14 @@
             this.ctlSearch = new System.Windows.Forms.Button();
             this.ctlSearchText = new System.Windows.Forms.TextBox();
             this.lblSearchFor = new System.Windows.Forms.Label();
+            this.ctlSearchResultsListView = new System.Windows.Forms.ListView();
+            this.colLineNumber = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colSearchMatchLine = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblStatusMessage = new System.Windows.Forms.ToolStripStatusLabel();
+            this.ctlFileOpenDialog = new System.Windows.Forms.OpenFileDialog();
+            this.fileSearchBackgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblFileName
@@ -53,6 +61,7 @@
             this.ctlFileName.Name = "ctlFileName";
             this.ctlFileName.Size = new System.Drawing.Size(506, 22);
             this.ctlFileName.TabIndex = 1;
+            this.ctlFileName.TextChanged += new System.EventHandler(this.ctlText_TextChanged);
             // 
             // ctlBrowseFile
             // 
@@ -63,6 +72,7 @@
             this.ctlBrowseFile.TabIndex = 2;
             this.ctlBrowseFile.Text = "Browse";
             this.ctlBrowseFile.UseVisualStyleBackColor = true;
+            this.ctlBrowseFile.Click += new System.EventHandler(this.ctlBrowseFile_Click);
             // 
             // ctlSearch
             // 
@@ -73,6 +83,7 @@
             this.ctlSearch.TabIndex = 5;
             this.ctlSearch.Text = "Search";
             this.ctlSearch.UseVisualStyleBackColor = true;
+            this.ctlSearch.Click += new System.EventHandler(this.ctlSearch_Click);
             // 
             // ctlSearchText
             // 
@@ -81,6 +92,7 @@
             this.ctlSearchText.Name = "ctlSearchText";
             this.ctlSearchText.Size = new System.Drawing.Size(506, 22);
             this.ctlSearchText.TabIndex = 4;
+            this.ctlSearchText.TextChanged += new System.EventHandler(this.ctlText_TextChanged);
             // 
             // lblSearchFor
             // 
@@ -92,11 +104,59 @@
             this.lblSearchFor.TabIndex = 3;
             this.lblSearchFor.Text = "Search for:";
             // 
+            // ctlSearchResultsListView
+            // 
+            this.ctlSearchResultsListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colLineNumber,
+            this.colSearchMatchLine});
+            this.ctlSearchResultsListView.Location = new System.Drawing.Point(17, 83);
+            this.ctlSearchResultsListView.MultiSelect = false;
+            this.ctlSearchResultsListView.Name = "ctlSearchResultsListView";
+            this.ctlSearchResultsListView.Size = new System.Drawing.Size(665, 420);
+            this.ctlSearchResultsListView.TabIndex = 6;
+            this.ctlSearchResultsListView.UseCompatibleStateImageBehavior = false;
+            this.ctlSearchResultsListView.View = System.Windows.Forms.View.Details;
+            // 
+            // colLineNumber
+            // 
+            this.colLineNumber.Text = "Line Number";
+            this.colLineNumber.Width = 94;
+            // 
+            // colSearchMatchLine
+            // 
+            this.colSearchMatchLine.Text = "Match line";
+            this.colSearchMatchLine.Width = 567;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblStatusMessage});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 510);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(700, 22);
+            this.statusStrip1.TabIndex = 7;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblStatusMessage
+            // 
+            this.lblStatusMessage.Name = "lblStatusMessage";
+            this.lblStatusMessage.Size = new System.Drawing.Size(0, 17);
+            // 
+            // ctlFileOpenDialog
+            // 
+            this.ctlFileOpenDialog.FileName = "openFileDialog1";
+            // 
+            // fileSearchBackgroundWorker
+            // 
+            this.fileSearchBackgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.fileSearchBackgroundWorker_DoWork);
+            // 
             // TextSearchForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(697, 515);
+            this.ClientSize = new System.Drawing.Size(700, 532);
+            this.Controls.Add(this.ctlSearchResultsListView);
+            this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.ctlSearch);
             this.Controls.Add(this.ctlSearchText);
             this.Controls.Add(this.lblSearchFor);
@@ -104,7 +164,11 @@
             this.Controls.Add(this.ctlFileName);
             this.Controls.Add(this.lblFileName);
             this.Name = "TextSearchForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Text Search";
+            this.Load += new System.EventHandler(this.TextSearchForm_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -118,6 +182,13 @@
         private System.Windows.Forms.Button ctlSearch;
         private System.Windows.Forms.TextBox ctlSearchText;
         private System.Windows.Forms.Label lblSearchFor;
+        private System.Windows.Forms.ListView ctlSearchResultsListView;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblStatusMessage;
+        private System.Windows.Forms.ColumnHeader colLineNumber;
+        private System.Windows.Forms.ColumnHeader colSearchMatchLine;
+        private System.Windows.Forms.OpenFileDialog ctlFileOpenDialog;
+        private System.ComponentModel.BackgroundWorker fileSearchBackgroundWorker;
     }
 }
 
--- Asg4_pxd160530/Asg4_pxd160530/Form1.cs
+++ Asg4_pxd160530/Asg4_pxd160530/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Asg4_pxd160530.IO;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,118 @@ namespace Asg4_pxd160530
 {
     public partial class TextSearchForm : Form
     {
+        bool isSearchOn;
+        Queue<SearchResult> resultsQueue;
+        string searchString;
+
         public TextSearchForm()
         {
             InitializeComponent();
         }
+
+        private void initializeForm()
+        {
+            ctlSearch.Enabled = false;
+            ctlSearchText.Text = "";
+            ctlFileName.Text = "";
+            ctlSearchResultsListView.Items.Clear();
+            isSearchOn = false;
+            resultsQueue.Clear();
+        }
+
+        /// <summary>
+        /// Method displays the given message in the status bar. 
+        /// It also resets the text color to Black indicating a normal message is being displayed.
+        /// </summary>
+        /// <param name="message">Message to display</param>
+        private void showMessage(String message)
+        {
+            lblStatusMessage.Text = message;
+        }
+
+        private void TextSearchForm_Load(object sender, EventArgs e)
+        {
+            initializeForm();
+            resultsQueue = new Queue<SearchResult>();
+        }
+
+        private void ctlBrowseFile_Click(object sender, EventArgs e)
+        {
+            DialogResult result = ctlFileOpenDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                if (!String.IsNullOrWhiteSpace(ctlFileOpenDialog.FileName))
+                {
+                    ctlFileName.Text = ctlFileOpenDialog.FileName;
+                }
+                else
+                {
+                    showMessage("Kindly select a valid file to search!");
+                }
+            }
+        }
+
+        private void ctlText_TextChanged(object sender, EventArgs e)
+        {
+            if(!String.IsNullOrWhiteSpace(ctlFileName.Text)
+                && !String.IsNullOrWhiteSpace(ctlSearchText.Text) 
+                && System.IO.File.Exists(ctlFileName.Text)
+                && !isSearchOn)
+            {
+                ctlSearch.Enabled = true;
+            }
+            else
+            {
+                ctlSearch.Enabled = false;
+            }
+        }
+
+        private void ctlSearch_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrWhiteSpace(ctlFileName.Text)
+                && !String.IsNullOrWhiteSpace(ctlSearchText.Text)
+                && System.IO.File.Exists(ctlFileName.Text))
+            {
+                if (!isSearchOn)
+                {
+                    searchString = ctlSearchText.Text;
+                    ctlFileName.Enabled = false;
+                    ctlSearchText.Enabled = false;
+                    showMessage("Starting search...");
+                    fileSearchBackgroundWorker.RunWorkerAsync();
+                }
+            }
+            else
+            {
+                showMessage("Kindly select a valid file to search or check search text!");
+            }
+        }
+
+        private void fileSearchBackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            FileReader reader = new FileReader(ctlFileName.Text);
+            if(reader.openFile())
+            {
+                string fileLine = "";
+                while (true)
+                {
+                    if (fileSearchBackgroundWorker.CancellationPending)
+                    {
+                        e.Cancel = true;
+                        fileSearchBackgroundWorker.ReportProgress(0);
+                        return;
+                    }
+                    fileLine = reader.readFromFile();
+                    if (fileLine == null) break;
+                    
+                }
+            }
+        }
+    }
+
+    class SearchResult
+    {
+        string matchLine { get; set; }
+        int lineNumber { get; set; }
     }
 }
